e6ebe0011c1c9b822f44f34dd3b19790bfe8a659
king <kanghua151@msn.com>
2023-10-24 19:41:54 +0800

    x

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ VERSION=v1.1.0
 
 rm -rf bin
 
-export GOOS=darwin
-export GOARCH=arm64
+#export GOOS=darwin
+#export GOARCH=arm64
 go build -trimpath -ldflags "-s -w" -o bin/azure-openai-proxy ./cmd
 
 #docker build -t stulzq/azure-openai-proxy:$VERSION .
